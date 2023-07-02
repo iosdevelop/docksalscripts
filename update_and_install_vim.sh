95be0a10457f05e9272ae23baca197e968f09c7a
Andre Morrison <morrison.andre@gmail.com>
2023-07-01 21:37:35 -0700

    remove symlinks

--- update_and_install_vim.sh
+++ update_and_install_vim.sh
@@ -8,3 +8,8 @@ apt-get upgrade -y
 
 # Install Vim
 apt-get install -y vim
+
+#remove 
+rm /var/www/fund/vendor/drush/drush/docs/contribute/CONTRIBUTING.md
+rm /var/www/fund/vendor/drush/drush/docs/drush_logo-black.png
+rm /var/www/fund/vendor/drush/drush/docs/img/favicon.ico
