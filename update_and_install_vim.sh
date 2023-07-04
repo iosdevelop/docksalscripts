f597b739c32ea7933ed38219baab3dc6b3cde660
Andre Morrison <morrison.andre@gmail.com>
2023-07-03 19:56:48 -0700

    locales

--- update_and_install_vim.sh
+++ update_and_install_vim.sh
@@ -8,7 +8,8 @@ apt-get upgrade -y
 
 # Install Vim
 apt-get install -y vim
-
+#path
+export LC_ALL=en_US.UTF-8
 #remove 
 rm /var/www/fund/vendor/drush/drush/docs/contribute/CONTRIBUTING.md
 rm /var/www/fund/vendor/drush/drush/docs/drush_logo-black.png
